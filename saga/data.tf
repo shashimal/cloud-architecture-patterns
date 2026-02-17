306f92d1284f618f4b47b653e4ae2f08f890b728
shashimal <shashimald@gmail.com>
2026-02-17 10:46:05 +0800

    Add charge-payment zip data

--- saga/data.tf
+++ saga/data.tf
@@ -10,6 +10,12 @@ data "archive_file" "cancel_order_zip" {
   output_path = "${path.module}/lambda/cancel-order.zip"
 }
 
+data "archive_file" "charge_payment_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda/charge-payment"
+  output_path = "${path.module}/lambda/charge-payment.zip"
+}
+
 data "aws_iam_policy_document" "lambda_permission" {
   statement {
     sid    = "DynamodbPermission"
